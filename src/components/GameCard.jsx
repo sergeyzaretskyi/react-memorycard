5c9c6bdadf3c3e8e00d20fcf1baad69ab63729af
Sergey Zaretskyi <sergeyzaretskyi@proton.me>
2023-04-21 19:31:20 +0300

    feat: Add function to handle click on card

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,9 +1,42 @@
 import { Card } from 'react-bootstrap';
-import { capitalizeFirstLetter } from '../utils/utils';
+import { animalsNeeded, capitalizeFirstLetter } from '../utils/utils';
 
-export const GameCard = ({ animal }) => {
+export const GameCard = ({
+  animal,
+  clickedAnimals,
+  setClickedAnimals,
+  setIsGameOver,
+  setIsGameStarted,
+  currentScore,
+  setCurrentScore,
+  highestScore,
+  setHighestScore,
+  currentLevel,
+  setCurrentLevel,
+}) => {
+  const onAnimalClick = (animal) => {
+    const isAnimalClicked = clickedAnimals.includes(animal);
+
+    if (isAnimalClicked) {
+      setIsGameOver(true);
+      setIsGameStarted(false);
+
+      if (currentScore > highestScore) {
+        setHighestScore(currentScore);
+      }
+    } else {
+      setClickedAnimals((prev) => [...prev, animal]);
+      setCurrentScore((prev) => prev + 1);
+
+      if (clickedAnimals.length + 1 === animalsNeeded(currentLevel)) {
+        setCurrentLevel((prev) => prev + 1);
+      }
+    }
+  };
+
   return (
     <Card
+      onClick={() => onAnimalClick(animal)}
       border="primary"
       style={{ width: '150px' }}
       className="bg-dark text-white hover"
